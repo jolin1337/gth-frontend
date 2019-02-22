f06f2af280a107f495ada858663f4762c6938420
Johannes Lindén <johannes@godesity.se>
2019-02-22 23:01:22 +0100

    Add login page

---
+++ src/screens/login/Login.tsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './Login.css';
+import logo from '../../logo.svg';
+
+class Login extends Component {
+  render() {
+    return (
+      <form className="LoginPage">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <label htmlFor="name">Name:</label>
+        <input required type="text" id="name" />
+        <label htmlFor="password">Password:</label>
+        <input required type="text" id="password" />
+        <input type="submit" value="Login" />
+        <a href="#" className="App-link Register-account">Register account</a>
+      </form>
+    );
+  }
+}
+
+export default Login;
