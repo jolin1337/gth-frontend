745c750106bd80a425dfbd93d068c0131c96f7b1
Kayi1500 <kayi1500@student.miun.se>
2019-02-23 01:26:17 +0100

    Added home view

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/home" component={Home} />
+
           </Switch>
         </Router>
       </div>
--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import './Home.css';
+import {Link } from "react-router-dom";
+
 
 class Home extends Component {
+
   render() {
     return (
-      <div>
-        Hej
+      <div className = "mainDivContainer">
+      	<Link to="" className="homeLinks">	
+      		Create new activity
+       	</Link>
+      	<Link to="" className="homeLinks">	
+  			Activities close to you
+       	</Link>
+       	<div className="plannedActivities">
+       		<p>My planned activities</p>
+       		<hr></hr>
+       			<ul>
+       				<li>Running</li>
+       				<li>Football</li>
+       				<li>Basketball</li>
+       				<li>Walk in the park</li>
+       				<li>Chill</li>
+       			</ul>
+       	</div>
       </div>
     );
   }
