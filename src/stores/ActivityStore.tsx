928c145f6f63f729918f61c7761c9380b16bf377
Marcus <Marcus@osynlig.se>
2019-02-23 07:43:25 +0100

    Fix minimal url bug

--- src/stores/ActivityStore.tsx
+++ src/stores/ActivityStore.tsx
@@ -10,27 +10,29 @@ class ActivityStore {
   @action
   public loadActivities = () => {
     this.isLoading = true;
-    fetch('http://localhost:3007/api/activities').then((a : any) => a.json())
-    .then((activities : any) => {
-      this.setActivities(activities);
-    });
+    fetch("http://localhost:3007/api/activities")
+      .then((a: any) => a.json())
+      .then((activities: any) => {
+        this.setActivities(activities);
+      });
   };
 
   @action
-  public setActivities = (activities : any) => {
+  public setActivities = (activities: any) => {
     this.activities = activities;
     this.isLoading = false;
   };
 
   @action
   public addActivity = async (activity: any) => {
-    const addedActivity = await fetch("https://localhost:3007/api/activity", {
+    console.log("adding", activity);
+    const addedActivity = await fetch("http://localhost:3007/api/activities", {
       method: "POST",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(activity)
+      body: JSON.stringify({ ...activity, userId: 1 })
     });
     console.log("addedActivity", addedActivity);
     this.activities.push(activity);
